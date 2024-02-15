8cbf8ba1648e2a96695ce6c24242f0fef7847488
Samy-666 <samy.hadj-ali@epitch.eu>
2024-02-15 11:08:15 +0100

    Ajout d'un sample de la configuration

---
+++ backend/config.sample.py
@@ -0,0 +1,28 @@
+import os
+
+SECRET_KEY = "ADD ME"
+
+
+#Database 
+DB_NAME = "ADD ME"
+
+DB_USERNAME = "ADD ME"
+
+DB_PASSWORD = "ADD ME"
+
+DB_PORT = "ADD ME"
+
+DB_HOST = "ADD ME"
+
+# Connect to the database
+SQLALCHEMY_DATABASE_URI = 'ADD ME'
+
+
+# Grabs the folder where the script runs.
+basedir = 'ADD ME' 
+
+# Enable debug mode.
+DEBUG = 'ADD ME'
+
+# Turn off the Flask-SQLAlchemy event system and warning
+SQLALCHEMY_TRACK_MODIFICATIONS = 'ADD ME'

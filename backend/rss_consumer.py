c05911703ff5190235f7a47d1083c7d4c9370c05
Samy-666 <samy.hadj-ali@epitch.eu>
2024-02-22 10:45:35 +0100

    modification du consumer rss

--- backend/rss_consumer.py
+++ backend/rss_consumer.py
@@ -1,6 +1,5 @@
 from kafka import KafkaConsumer
 import json
-# Configuration du consommateur Kafka
 
 # Configuration Kafka
 topic_name = "rss-topic"
@@ -11,26 +10,15 @@ consumer = KafkaConsumer(
     bootstrap_servers=[kafka_server],
     auto_offset_reset='earliest',
     group_id='myFirst-group',
-    value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+    value_deserializer=lambda x: json.loads(x.decode('utf-8')),
+    consumer_timeout_ms=1000  # Définir le délai d'attente ici
 )
-# Abonnement au topic Kafka
-consumer.subscribe(['rss-topic'])
 
 # Boucle de consommation des messages
-while True:
-    msg = consumer.poll(timeout=1.0)
-    if msg is None:
-        continue
-    if msg.error():
-        if msg.error().code() == 'PARTITION_EOF':
-            # Fin de la partition, continuons
-            continue
-        else:
-            # Erreur irrécupérable, arrêtons le consommateur
-            print(msg.error())
-            break
-    # Affichage du message
-    print('Received message: {}'.format(msg.value().decode('utf-8')))
-
-# Fermeture du consommateur Kafka
-consumer.close()
+try:
+    for msg in consumer:
+        print('Received message: {}'.format(msg.value.decode('utf-8')))
+except KeyboardInterrupt:
+    pass
+finally:
+    consumer.close()

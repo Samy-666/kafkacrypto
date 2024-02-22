2a3e1c3bef140c41368177ac534bcbca3ac00b90
Samy-666 <samy.hadj-ali@epitch.eu>
2024-02-22 10:29:58 +0100

    ajout d'un autre topic

---
+++ backend/rss_consumer.py
@@ -0,0 +1,31 @@
+from kafka import Consumer, KafkaError
+
+# Configuration du consommateur Kafka
+conf = {'bootstrap.servers': "localhost:9092",
+        'group.id': "my_consumer_group",
+        'auto.offset.reset': 'earliest'}
+
+# Création de l'instance du consommateur
+consumer = Consumer(conf)
+
+# Abonnement au topic Kafka
+consumer.subscribe(['rss-topic'])
+
+# Boucle de consommation des messages
+while True:
+    msg = consumer.poll(timeout=1.0)
+    if msg is None:
+        continue
+    if msg.error():
+        if msg.error().code() == KafkaError._PARTITION_EOF:
+            # Fin de la partition, continuons
+            continue
+        else:
+            # Erreur irrécupérable, arrêtons le consommateur
+            print(msg.error())
+            break
+    # Affichage du message
+    print('Received message: {}'.format(msg.value().decode('utf-8')))
+
+# Fermeture du consommateur Kafka
+consumer.close()
---
+++ backend/rss_producer.py
@@ -0,0 +1,25 @@
+from kafka import Producer
+import feedparser 
+
+# Configuration du producteur Kafka
+conf = {'bootstrap.servers': "localhost:9092"}
+
+# Création de l'instance du producteur
+producer = Producer(conf)
+
+# URL du flux RSS à récupérer
+rss_feed_url = "https://coinjournal.net/fr/actualites/tag/"
+
+# Fonction de publication des messages dans Kafka
+def publish_to_kafka(topic, message):
+    producer.produce(topic, value=message)
+    producer.flush()
+
+# Fonction pour récupérer les entrées du flux RSS et les publier dans Kafka
+def fetch_and_publish_rss(rss_feed_url, kafka_topic):
+    feed = feedparser.parse(rss_feed_url)
+    for entry in feed.entries:
+        publish_to_kafka(kafka_topic, entry.title)
+
+# Appel de la fonction pour récupérer et publier les messages
+fetch_and_publish_rss(rss_feed_url, "rss-topic")
